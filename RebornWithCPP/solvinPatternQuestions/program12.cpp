0416c045f467eedf09f68aca18a9dd7a8d4856bc
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2026-02-06 06:19:55 +0000

    Committed!

---
+++ RebornWithCPP/solvinPatternQuestions/program12.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  int n;
+  cout<<"Enter the value of n: ";
+  cin>>n;
+
+  char ch = 'A';
+
+  for(int i = 0 ; i < n ; i++){
+    char ch1 = ch++;
+    for(int j = 0 ; j < n ; j++){
+      printf("%3c", ch1);
+    }
+    putchar('\n');
+  }
+  return 0;
+}
