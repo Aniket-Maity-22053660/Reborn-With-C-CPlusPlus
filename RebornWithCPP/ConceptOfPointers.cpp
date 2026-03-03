628c59b7cc0ef7709e7f991ead054b87c14c99a6
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2026-03-03 09:17:10 +0000

    Committed!

--- RebornWithCPP/ConceptOfPointers.cpp
+++ RebornWithCPP/ConceptOfPointers.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include<cstring>
 
 using namespace std;
 
+int calcStrLength(char* name);
 int main(){
 
   int num[] = {10, 20, 30, 40, 50};
@@ -32,5 +34,41 @@ int main(){
     cout<<*(*(*(*(num4 + 0))) + i)<<" ";
   }
   putchar('\n');
+  int arr[4][4] = {{1, 2, 3, 4},
+		   {5, 6, 7, 8},
+		   {9, 10, 11, 12},
+		   {13, 14, 15, 16}
+                  };
+  cout<<"(arr + 0): "<<(arr + 0)<<endl;
+  cout<<"*(arr + 0): "<<*(arr + 0)<<endl;
+  int (*ptr)[4] = arr;
+  cout<<(ptr + 0)<<endl;
+  for(int i = 0 ; i < 4 ; i++){
+    for(int j = 0 ; j < 4 ; j++){
+      printf("%3d", *(*(ptr + i) + j));
+    }
+    cout<<'\n';
+  }
+  int arr2[] = {10, 20, 30, 40, 50};
+  int (*ptr1)[5] = &arr2;
+  for(int i = 0 ; i < 4 ; i++){
+    cout<<*(*(ptr1 + 0) + i)<<" ";
+  }
+  cout<<'\n';
+  cout<<arr2<<endl<<&arr2<<endl;
+  char name[30] = "Aniket Maity!";
+  cout<<name<<": "<<calcStrLength(name)<<endl;
+  char* name1 = new char[100];
+  cout<<"Enter your name: ";
+  cin.getline(name1, 100);
+  cout<<"Your  name is "<<name1<<endl;
+  //name = "King star khan!";
+  strcpy(name, "King star khan!");
+  cout<<name<<": "<<calcStrLength(name)<<endl;
   return 0;
+}
+
+int calcStrLength(char* name){
+  int length = strlen(name);
+  return length;
 }
