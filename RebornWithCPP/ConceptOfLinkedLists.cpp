3a2951f3ec22dcfbee3ac229e0192bb7bc5496da
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2026-03-04 18:06:48 +0000

    \Committed!

--- RebornWithCPP/ConceptOfLinkedLists.cpp
+++ RebornWithCPP/ConceptOfLinkedLists.cpp
@@ -55,6 +55,7 @@ void insertAtPos(Node* head, int position, int data){
     head->next = node;
     node->prev = head;
     node->next = temp;
+    Node::count++;
     return;
   }
   return;
@@ -85,7 +86,59 @@ void printNodes(Node* head){
   return;
 }
 
+Node* deleteAtHead(Node* head){
+  if(head != NULL){
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    Node::count--;
+    return head;
+  }
+  return head;
+}
 
+void deleteAtTail(Node* head){
+  if(head == NULL){
+    return;
+  }
+  if(head->next->next != NULL){
+    deleteAtTail(head->next);
+  }else{
+    Node* temp = head->next;
+    head->next = NULL;
+    delete temp;
+    Node::count--;
+    return;
+  }
+  return;
+}
+
+void delAtPos(Node* head, int position){
+  if(position != 2){
+    delAtPos(head->next, position - 1);
+  }else{
+    Node* temp = head->next->next;
+    Node* temp1 = head->next;
+    head->next = temp;
+    temp->prev = head;
+    Node::count--;
+    delete temp1;
+  }
+  return;
+}
+
+void deleteAtPos(Node** head, int position){
+  if(position == 1){
+    *head = deleteAtHead(*head);
+    return;
+  }
+  if(position == Node::count){
+    deleteAtTail(*head);
+    return;
+  }
+  delAtPos(*head, position);
+  return;
+}
 
 int Node::count = 0;
 
@@ -101,6 +154,14 @@ int main(){
   insertAtPosition(&head, 4, 50);
   printNodes(head);
   insertAtPosition(&head, 1, 100);
+  printNodes(head);
+  insertAtPosition(&head, 2, 60);
+  printNodes(head);
+  head = deleteAtHead(head);
+  printNodes(head);
+  deleteAtTail(head);
+  printNodes(head);
+  deleteAtPos(&head, 3);
   printNodes(head);
   return 0;
 }
