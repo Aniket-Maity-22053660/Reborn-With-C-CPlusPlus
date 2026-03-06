f341ab75dc3a540e7bbe4d21502bd1510300fe19
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2026-03-06 06:22:15 +0000

    Committed!

--- RebornWithCPP/merge2SortedList.cpp
+++ RebornWithCPP/merge2SortedList.cpp
@@ -45,16 +45,70 @@ void printNodes(Node* head){
   return;
 }
 
+void merge(Node** head1, Node** head2, Node** head3){
+  if(*head1 != NULL && *head2 != NULL){
+    if((*head1)->data < (*head2)->data){
+      insert(head3, (*head1)->data);
+      *head1 = (*head1)->next;
+    }else{
+      insert(head3, (*head2)->data);
+      *head2 = (*head2)->next;
+    }
+    merge(head1, head2, head3);
+    return;
+  }
+  return;
+}
+
+void mergeList1(Node* head1, Node** head3){
+  if(head1 != NULL){
+    insert(head3, head1->data);
+    mergeList1(head1->next, head3);
+    return;
+  }
+  return;
+}
+
+void mergeList2(Node* head2, Node** head3){
+  if(head2 != NULL){
+    insert(head3, head2->data);
+    mergeList2(head2->next, head3);
+    return;
+  }
+  return;
+}
+
+Node* merge2List(Node* head1, Node* head2){
+  Node* head3 = NULL;
+  merge(&head1, &head2, &head3);
+  mergeList1(head1, &head3);
+  mergeList2(head2, &head3);
+  return head3;
+}
+
 int main(){
-  Node* head = NULL;
+  Node* head1 = NULL, *head2 = NULL, *head3 = NULL;
   int num, element;
-  cout<<"Enter total number of nodes in the LinkedList: ";
+  cout<<"Enter total number of nodes in the LinkedList - 1: ";
   cin>>num;
   for(int i = 0 ; i < num ; i++){
     cout<<"Enter element - "<<(i + 1)<<": ";
     cin>>element;
-    insert(&head, element);
+    insert(&head1, element);
   }
-  printNodes(head);
+  cout<<"List - 1: ";
+  printNodes(head1);
+  cout<<"Enter total number of nodes in the LinkedList - 2: ";
+  cin>>num;
+  for(int i = 0 ; i < num ; i++){
+    cout<<"Enter element - "<<(i + 1)<<": ";
+    cin>>element;
+    insert(&head2, element);
+  }
+  cout<<"List - 2: ";
+  printNodes(head2);
+  head3 = merge2List(head1, head2);
+  cout<<"After merging two lists: ";
+  printNodes(head3);
   return 0;
 }
